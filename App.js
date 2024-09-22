2e659a54d866f39dcf6bb06f2bc13f035f5ccf1e
k7 <kesavanvel77@gmail.com>
2024-09-22 23:30:54 +0530

    food ordering UI Body

--- App.js
+++ App.js
@@ -1,42 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./public/assets/logo.png";
+import "./app.css";
+import responseData from './public/json/List.json';
 
-const heading = React.createElement("h1", { id: "heading" }, "I am ready");
-
-/* JXS transpiled before it reaches the js - Done by parcel - babel
-JXS => React.createElement => object => HTMLElement(render);
-jsx - is not a HTML inside js */
-const jsx = <h1>Hello guy</h1>;
-
-// React element
-const alterHeading = (
-  <div>
-    <h1>I am ready</h1>
-  </div>
-);
-
-//React functional component 1
-const HeadingComponent1 = () => {
+const Header = () => {
   return (
-    <div>
-      <h1>This is my first component</h1>
-    </div>
-  );
-};
-
-//React functional component 2
-// react component composition (conponent with another component)
-const HeadingComponent2 = () => {
-  return (
-    <div>
-      <HeadingComponent1 />
-      <div>
-        <h1>This is my second component</h1>
+    <div className="header">
+      <div className="header-logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="header-items">
+        <ul className="items-list">
+          <li>Home</li>
+          <li>About</li>
+          <li>Orders</li>
+          <li>Cart</li>
+        </ul>
       </div>
     </div>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const ItemCard = (props) => {
+  console.log(props.data);
+  return (
+    <div className="items-body">
+      {props.data.map((restaurant) => (
+        <div className="item-card" key={restaurant.info.id}>
+          <img
+            src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurant.info.cloudinaryImageId}`} // Update with actual image URL
+            alt={`${restaurant.info.name} food`}
+          />
+          <h3>{restaurant.info.name}</h3>
+          <p>{restaurant.info.cuisines.join(",") || "Default Food Name"}</p>
+          <p>Price: {restaurant.info.costForTwo || "Price not available"}</p>
+          <p>Ratings: {restaurant.info.avgRating || "No ratings available"}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-root.render(<HeadingComponent2 />);
+const Body = () => {
+  return (
+    <div>
+      <div className="search-body">
+        <input type="search" placeholder="Search items" />
+      </div>
+      <p className="item-head">Restaurants</p>
+        <ItemCard data = {responseData.data.cards[0].card.card.gridElements.infoWithStyle.restaurants}/>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  console.log(responseData.data.cards[0].card.card.gridElements.infoWithStyle.restaurants);
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
